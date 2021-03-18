2b374efcbaf84e33bd8d659bc74230da0fc55d8f
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-03-17 19:46:48 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+//arrow function to divide
+let divide = 2000 => 2000 / 100;
+
+//arrow function multiply
+let square = 2 => 2 * 2;
+
+//arrow function to add
+let a = 3;
+let b = 4;
+let add = a => a + b;
+
+
+
